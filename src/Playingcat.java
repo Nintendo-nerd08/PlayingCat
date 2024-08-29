433c1de5a7c4b755e99572f3fe43ca36edd03070
lloydkev000 <lloydkevin631@gmail.com>
2024-08-29 13:28:11 -0400

    Finished!

--- src/Playingcat.java
+++ src/Playingcat.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Playingcat {
+public class Playingcat {
+    public static void main(String[] args) {
+        System.out.println(isCatPlaying(true, 10));
+        System.out.println(isCatPlaying(false, 36));
+        System.out.println(isCatPlaying(false, 35));
+
+    }
+
+    private static boolean isCatPlaying(boolean summer, int temperature) {
+        if (summer) {
+            if (temperature >= 25 && temperature <= 45) {
+                return true;
+            }
+            return false;
+        } else {
+            if (temperature >= 25 && temperature <= 35) {
+                return true;
+            }
+            return false;
+
+        }
+    }
 }
